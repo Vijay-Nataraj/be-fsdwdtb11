cd4e4abb43cfc7126358fbcf5cc008f3f9186650
Vijay-Nataraj <victoryvijayn@gmail.com>
2024-12-26 20:12:23 +0530

    common JS import

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.mjs
-import { createServer } from "node:http";
+const createServer = require("http");
 
 const server = createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
