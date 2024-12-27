7b52cdc584561cf1d819c38b1e0b400d29e5f6f2
Vijay-Nataraj <victoryvijayn@gmail.com>
2024-12-27 19:17:13 +0530

    writing apis using expressjs

--- server.js
+++ server.js
@@ -1,35 +1,55 @@
-// import http module to setup http server
-const http = require("http");
+// import express module
+const express = require("express");
 
-//create a server object
-const server = http.createServer((request, response) => {
-  //set the response header and  the response body
-  //for plain text
-  // response.writeHead(200, { "content-type": "text/plain" });
-  // response.end("Hello world\n");
-  //for html
-  // response.writeHead(200, { "content-type": "text/html" });
-  // response.end("<h1>Hello World</h1>");
-  //for json
-  // response.writeHead(200, { "content-type": "Application/json" });
-  // response.end(JSON.stringify({ message: "Hello World" }));
+//create a express app
+const app = express();
 
-  response.writeHead(200, { "content-type": "Application/json" });
-  if (request.url === "/posts") {
-    //endpoints for posts
-    if (request.method === "GET") {
-      response.write(JSON.stringify({ Message: "GET POSTS" }));
-    } else if (request.method === "POST") {
-      response.write(JSON.stringify({ Message: "POST POSTS" }));
-    }
-  } else if (request.url === "/comments") {
-    //endpoints for comments
-    response.write(JSON.stringify({ Message: "Comments" }));
-  }
-  response.end();
+app.get("/", (request, response) => {
+  response.json({ Message: "GET /" });
 });
 
-// listen to the server on port 3000
-server.listen(3000, "127.0.0.1", () => {
+app.post("/", (request, response) => {
+  response.json({ Message: "POST /" });
+});
+
+app.listen(3000, () => {
   console.log("Server is running on http://127.0.0.1:3000");
 });
+
+// //import http module to setup http server
+// const server = http.createServer((request, response) => {
+//   //set the response header and  the response body
+//   //for plain text
+//   // response.writeHead(200, { "content-type": "text/plain" });
+//   // response.end("Hello world\n");
+//   //for html
+//   // response.writeHead(200, { "content-type": "text/html" });
+//   // response.end("<h1>Hello World</h1>");
+//   //for json
+//   // response.writeHead(200, { "content-type": "Application/json" });
+//   // response.end(JSON.stringify({ message: "Hello World" }));
+
+//   response.writeHead(200, { "content-type": "Application/json" });
+//   if (request.url === "/posts") {
+//     //endpoints for posts
+//     if (request.method === "GET") {
+//       response.write(JSON.stringify({ Message: "GET POSTS" }));
+//     } else if (request.method === "POST") {
+//       response.write(JSON.stringify({ Message: "POST POSTS" }));
+//     }
+//   } else if (request.url === "/comments") {
+//     //endpoints for comments
+//     if (request.method === "GET") {
+//       response.write(JSON.stringify({ Message: "Comments POSTS" }));
+//     } else if (request.method === "POST") {
+//       response.write(JSON.stringify({ Message: "Comments POSTS" }));
+//     }
+//     response.write(JSON.stringify({ Message: "Comments" }));
+//   }
+//   response.end();
+// });
+
+// // listen to the server on port 3000
+// server.listen(3000, "127.0.0.1", () => {
+//   console.log("Server is running on http://127.0.0.1:3000");
+// });
